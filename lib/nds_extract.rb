c406623c16ec7f7f8abfdff0aaf779167dbbb897
Christina McMahon <christina.mcmahon1@gmail.com>
2019-11-21 23:52:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,7 +46,13 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  
+  total = 0
+  directors = list_of_directors(source)
+  director_index = 0 
+  while director_index < source.size do
+    total += source[]
+    director_index += 1
+  end
 end
 
 
